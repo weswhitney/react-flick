06c214995be1e0c3c283857688ae8d1a155739a8
weswhitney <wesleywhitney@gmail.com>
2016-12-18 22:25:31 -0700

    react doc tutorial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 //import './App.css';
 import MainComponent from './components/MainComponent.js';
-
+import Game from './components/Game.js'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainComponent />
+        <Game />
       </div>
     );
   }
