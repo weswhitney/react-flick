5f2379d8b4e797ab3ab93d07c80053d28f883366
weswhitney <wesleywhitney@gmail.com>
2016-12-18 21:47:05 -0700

    added components folder

---
+++ src/components/MainComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class MainComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hellow world</h2>
+      </div>
+    );
+  }
+}
+
+export default MainComponent;
