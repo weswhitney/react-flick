cec57594cf6d158774463477a7d434305944667a
weswhitney <wesleywhitney@gmail.com>
2016-12-30 13:13:49 -0700

    commented out main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 //import './App.css';
 import MainComponent from './components/MainComponent.js';
+import Game from './components/Game.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainComponent />
+        <Game />
+         {/* <MainComponent /> */}
       </div>
     );
   }
